5be6c37bc703770a8055e14969d2b03880f91b85
jacovzap-assuresoft <jacobo.covarrubias@training.assuresoft.com>
2023-11-01 17:23:52 -0400

    formated address controller file

--- src/controllers/address.controller.js
+++ src/controllers/address.controller.js
@@ -1,11 +1,11 @@
 import { getAllAdressesRepository } from '../repositories/address.repository.js'
 
 export const getAllAdresses = async (req, res) => {
-    try {
-      const addresses = await getAllAdressesRepository()
-      res.status(200).json(addresses)
-    } catch (err) {
-      console.log(err.message)
-      res.status(500).json({ message: err.message })
-    }
+  try {
+    const addresses = await getAllAdressesRepository()
+    res.status(200).json(addresses)
+  } catch (err) {
+    console.log(err.message)
+    res.status(500).json({ message: err.message })
   }
+}
