cc02e2f5f84800a9b6e56465702c25b38e46ac8e
JoDemVel <josuedemetriovelizescobar@gmail.com>
2023-11-02 21:08:51 -0400

    Add crud methods and other operations in payment.repository.js

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ import morgan from 'morgan'
 import addressRouter from './routes/address.route.js'
 import storeRouter from './routes/store.route.js'
 import saleRouter from './routes/sale.route.js'
+import paymentRouter from './routes/payment.route.js'
 
 const app = express()
 
@@ -15,5 +16,6 @@ app.use(express.json())
 app.use('/address', addressRouter)
 app.use('/store', storeRouter)
 app.use('/sale', saleRouter)
+app.use('/payment', paymentRouter)
 
 export default app
