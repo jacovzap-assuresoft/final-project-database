a515630b165792e361fd1429d1a1d58e8815f6c2
JoseLozadaAD <jose.lozada@training.assuresoft.com>
2023-11-03 12:09:44 -0400

    Change var names

--- src/controllers/salary.controller.js
+++ src/controllers/salary.controller.js
@@ -6,53 +6,53 @@ import {
   updateSalaryRepository
 } from '../repositories/salary.repository.js'
 
-export const getAllSalaries = async (request, response) => {
+export const getAllSalaries = async (req, res) => {
   try {
     const salaries = await getAllSalariesRepository()
-    response.status(200).json(salaries)
+    res.status(200).json(salaries)
   } catch (error) {
-    response.status(500).json({ message: error.message })
+    res.status(500).json({ message: error.message })
   }
 }
 
-export const getSingleSalary = async (request, response) => {
+export const getSingleSalary = async (req, res) => {
   try {
-    const { id } = request.params
+    const { id } = req.params
     const salary = await getSingleSalaryRepository(Number(id))
-    response.status(200).json(salary)
+    res.status(200).json(salary)
   } catch (error) {
-    response.status(500).json({ message: error.message })
+    res.status(500).json({ message: error.message })
   }
 }
 
-export const createSalary = async (request, response) => {
+export const createSalary = async (req, res) => {
   try {
-    const data = request.body
+    const data = req.body
     const salaryCreated = await createSalaryRepository(data)
-    response.status(201).json(salaryCreated)
+    res.status(201).json(salaryCreated)
   } catch (error) {
-    response.status(500).json({ message: error.message })
+    res.status(500).json({ message: error.message })
   }
 }
 
-export const updateSalary = async (request, response) => {
+export const updateSalary = async (req, res) => {
   try {
-    const { id } = request.params
-    const data = request.body
+    const { id } = req.params
+    const data = req.body
     data.salary_id = Number(id)
     const salaryUpdated = await updateSalaryRepository({ data, id: Number(id) })
-    response.status(201).json(salaryUpdated)
+    res.status(201).json(salaryUpdated)
   } catch (error) {
-    response.status(500).json({ message: error.message })
+    res.status(500).json({ message: error.message })
   }
 }
 
-export const deleteSalary = async (request, response) => {
+export const deleteSalary = async (req, res) => {
   try {
-    const { id } = request.params
+    const { id } = req.params
     const salaryDeleted = await deleteSalaryRepository(Number(id))
-    response.status(200).json(salaryDeleted)
+    res.status(200).json(salaryDeleted)
   } catch (error) {
-    response.status(500).json({ message: error.message })
+    res.status(500).json({ message: error.message })
   }
 }
