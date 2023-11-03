6abffff3804e6f174c7c102c2c50f22de99b1c03
Juan Pablo Chavez Arteaga <juanpablo.chavez@training.assuresoft.com>
2023-11-03 13:23:38 -0400

    Code clean up

--- src/controllers/store.controller.js
+++ src/controllers/store.controller.js
@@ -42,7 +42,6 @@ export const updateStore = async (req, res) => {
   try {
     const { id } = req.params
     const data = req.body
-    console.log(data)
     const store = await updateStoreRepository(Number(id), data)
     res.status(200).json(store)
   } catch (err) {
